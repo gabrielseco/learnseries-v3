be0cbe94f8b538660ddf65e5121a91820785efbc
Gabriel García Seco <ggarciaseco@gmail.com>
2018-08-07 14:49:20 +0200

    Implicit any false for build in server

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
+import * as React from 'react';
 import { Query } from 'react-apollo';
 import { Layout } from 'containers';
 import { getFilms } from 'globals/queries';
 import { Film } from './../../shared/types';
 
 export default () => (
-  <Layout>
-    <Query query={getFilms}>
-      {({ loading, error, data }) => {
-        if (loading) return <p>Loading...</p>;
-        if (error) return <p>Error :(</p>;
-        return data.films.map(({ id, name, artwork, year, createdAt }: Film) => (
-          <div key={id}>
-            <img src={artwork} alt={name} />
-            <p>{name}</p>
-            <p>{year}</p>
-            <p>{createdAt}</p>
-          </div>
-        ));
-      }}
-    </Query>
-  </Layout>
+  <React.StrictMode>
+    <Layout>
+      <Query query={getFilms}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error :(</p>;
+          return data.films.map(({ id, name, artwork, year, createdAt }: Film) => (
+            <div key={id}>
+              <img src={artwork} alt={name} />
+              <p>{name}</p>
+              <p>{year}</p>
+              <p>{createdAt}</p>
+            </div>
+          ));
+        }}
+      </Query>
+    </Layout>
+  </React.StrictMode>
 );
